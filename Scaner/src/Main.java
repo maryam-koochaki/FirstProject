3eb8a91cd39c70a534350a6be693a8e9f71b1899
maryam-koochaki <59407001+maryam-koochaki@users.noreply.github.com>
2021-06-27 17:26:31 +0430

    Update Main.java

--- Scaner/src/Main.java
+++ Scaner/src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner scannr = new Scanner(System.in);
         System.out.println("Enter score:");
-        int score = s.nextInt();
+        int score = scannr.nextInt();
         if(score < 10){
             System.out.println("Failed");
         }else {
